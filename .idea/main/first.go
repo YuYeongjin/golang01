222fd1e062ac7a4da9bc016b91e8dd864a613fd7
유영진 <svvo2002@naver.com>
2022-12-23 15:28:23 +0900

    first go

--- .idea/main/first.go
+++ .idea/main/first.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	var a int
+	var b int
 	fmt.Println("Hello World!")
+	fmt.Println(plus(a, b))
+
+}
+
+func plus(a int, b int) int {
+	a = 500
+	b = 500
+
+	return a + b
+
 }
